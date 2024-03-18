cd694d676ffb98792668940b2c691e57b4080ac4
vjvansh0707 <vansh.jain9352742364@gmail.com>
2024-03-18 13:43:40 +0530

    this is the folder

---
+++ myapp/android/app/src/main/kotlin/com/vansh/myapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.vansh.myapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ myapp/lib/calculator.dart
@@ -0,0 +1,149 @@
+import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
+
+class Calculator extends StatefulWidget {
+  const Calculator({super.key});
+
+  @override
+  State<Calculator> createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  String statement = "";
+  String result = "0";
+  final List<String> buttons = [
+    'C',
+    '(',
+    ')',
+    '/',
+    '7',
+    '8',
+    '9',
+    '*',
+    '4',
+    '5',
+    '6',
+    '+',
+    '1',
+    '2',
+    '3',
+    '-',
+    'AC',
+    '0',
+    '.',
+    '=',
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            Flexible(flex: 2, child: resultWidget()),
+            Expanded(flex: 4, child: _buttons()),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget resultWidget() {
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Container(
+          padding: const EdgeInsets.all(20),
+          alignment: Alignment.centerRight,
+            child: Text(
+              statement,
+              style: const TextStyle(fontSize: 32),
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(15),
+          alignment: Alignment.centerRight,
+            child: Text(
+              result,
+              style: const TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _buttons() {
+    return GridView.builder(
+      gridDelegate:
+          const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
+      itemBuilder: (BuildContext context, int index) {
+        return _myButton(buttons[index]);
+      },
+      itemCount: buttons.length,
+    );
+  }
+
+  _myButton(String text) {
+    return Container(
+      margin: const EdgeInsets.all(8),
+      child: MaterialButton(
+        onPressed: () {
+          setState(() {
+            handleButtonTap(text);
+          });
+        },
+        color: _getColor(text),
+        textColor: Colors.white,
+        shape: const CircleBorder(),
+        child: Text(
+          text,
+          style: const TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+
+  handleButtonTap(String text) {
+    if (text == "AC") {
+      statement = "";
+      result = "0";
+      return;
+    }
+    if (text == "=") {
+      result = calculate();
+      if (result.endsWith(".0")) {
+        result = result.replaceAll(".0", "");
+      }
+      return;
+    }
+
+    if (text == "C") {
+      statement = statement.substring(0, statement.length - 1);
+      return;
+    }
+    statement = statement + text;
+  }
+
+  calculate() {
+    try {
+      var exp = Parser().parse(statement);
+      var evaluation = exp.evaluate(EvaluationType.REAL, ContextModel());
+      return evaluation.toString();
+    } catch (e) {
+      return "Err";
+    }
+  }
+
+  _getColor(String text) {
+    if (text == "/" || text == "*" || text == "+" || text == "-") {
+      return Colors.blue;
+    }
+    if (text == "C" || text == "AC") {
+      return Colors.red;
+    }
+    if (text == "(" || text == ")") {
+      return Colors.orange;
+    }
+    return Colors.black;
+  }
+}
